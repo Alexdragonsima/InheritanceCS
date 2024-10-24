6af480ab43180de0a88425b8419bad7896f4d5b3
Алексей Семионов <alehaaaaa@mail.ru>
2024-10-24 16:15:57 +0300

    Triengles

--- AbstractGeometry/Program.cs
+++ AbstractGeometry/Program.cs
@@ -29,8 +29,14 @@ namespace AbstractGeometry
 			Square square = new Square(75, 512, 15, 5, Color.DarkBlue);
 			square.Info(e);
 
-			Circle circle=new Circle(50,600,15,5, Color.Yellow);
+			Circle circle = new Circle(50, 600, 15, 5, Color.Yellow);
 			circle.Info(e);
+
+			IsoscalesTriangle i_triangle = new IsoscalesTriangle(50, 100, 700, 15, 5, Color.Green);
+			i_triangle.Info(e);
+
+			EquilateralTriangle e_triangle = new EquilateralTriangle(100, 400, 100, 3, Color.YellowGreen);
+			e_triangle.Info(e);
 		}
 		[DllImport("kernel32.dll")]
 		public static extern IntPtr GetConsoleWindow();
---
+++ AbstractGeometry/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbstractGeometry
+{
+	abstract class Triangle:Shape
+	{
+		public abstract double GetHeight();
+		public Triangle(int startX,int startY,int lineWidth,Color color):base(startX, startY, lineWidth, color) { }
+		public override void Info(PaintEventArgs e)
+		{
+            Console.WriteLine($"Высото треугольника: {GetHeight()}");
+			base.Info(e);
+		}
+	}
+}
